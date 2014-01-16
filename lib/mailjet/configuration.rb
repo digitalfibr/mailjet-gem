3d8c518e5cf524930b3323f59e86e287b19ced1b
Maxime <maxime.dev@gmail.com>
2014-01-16 20:43:40 +0100

    mattr_accessor to attr_accessor for use different client

--- lib/mailjet/configuration.rb
+++ lib/mailjet/configuration.rb
@@ -2,15 +2,16 @@ require 'active_support/core_ext/module/attribute_accessors'
 
 module Mailjet
   module Configuration
+    class << self
+      attr_accessor :api_key
+      attr_accessor :secret_key
+      attr_accessor :default_from
+      attr_accessor :domain
+    end
     mattr_accessor :api_version
-    mattr_accessor :api_key
-    mattr_accessor :secret_key
     mattr_accessor :use_https
-    mattr_accessor :domain
-    mattr_accessor :default_from
     
     @@use_https = true
     @@api_version = 0.1
-    @@domain = ''
   end
 end
